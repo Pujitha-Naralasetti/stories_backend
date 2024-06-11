1acbebc6c59f3d1885b65eab222a1d6ef92a50db
JagadeeswariAdapa <jagadeeswari.adapa@eagles.oc.edu>
2024-06-10 20:11:37 -0500

    Story deletion changes

--- app/routes/stories.routes.js
+++ app/routes/stories.routes.js
@@ -2,24 +2,28 @@ module.exports = (app) => {
   const Story = require("../controllers/stories.controller.js");
   const { authenticateRoute } = require("../authentication/authentication");
   var router = require("express").Router();
-
   // Create story
   router.post("/stories", [authenticateRoute], Story.create)
- // findAllStories
 
- router.get("/stories/",[authenticateRoute], Story.findAll)
+  // findAllStories
 
- // findAllStoriesByUserId
+  router.get("/stories/",[authenticateRoute], Story.findAll)
 
- router.get("/stories/User/:id",[authenticateRoute], Story.findAllByUserId)
+  // findAllStoriesByUserId
 
- // find story by id
+  router.get("/stories/User/:id",[authenticateRoute], Story.findAllByUserId)
 
- router.get("/stories/:id",[authenticateRoute], Story.findOne)
- 
+  // find story by id
+
+  router.get("/stories/:id",[authenticateRoute], Story.findOne)
+
   // update story  
 
   router.put("/stories/:id", [authenticateRoute], Story.update)
+
+
+  // Delete a storiy with id
+  router.delete("/stories/:id", [authenticateRoute], Story.delete);
 
   // Retrieve a all the story properties
   router.get("/storyProperties", [authenticateRoute], Story.getStoryProperties);
